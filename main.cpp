69990a00e4575854a66497908b2e0cb029178c5d
Rishabh Chaturvedi <118957285+rish7897@users.noreply.github.com>
2025-05-15 08:07:02 +0530

    added 2 methods of exponentiation. 1st the slower one takes O(n) time complexity and fast exponentiation takes O(logn) timecomplexity.

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int fastExponentiationRecurssion(int a, int b) {
+    if (b == 1) {
+        return a;
+    }
+    if (b == 0) {
+        return 1;
+    }
+    int smallAns = fastExponentiationRecurssion(a, b / 2);
+    int ans = smallAns * smallAns;
+    if (b & 1) {
+        ans *= a;
+    }
+    return ans;
+}
+
+int fastExponentiation(int a, int b) {
+    int ans = 1;
+    while (b > 0) {
+        if (b & 1) {
+            ans *= a;
+        }
+        a *= a;
+        b >>= 1;
+    }
+    return ans;
+}
+
+int slowExponentiation(int a, int b) {
+    int ans = 1;
+    for (int i = 0; i < b; i++) {
+        ans *= a;
+    }
+    return ans;
+} //O(b)
+
+int main() {
+    int a, b;
+    cout << "Enter a" << endl;
+    cin >> a;
+    cout << "Enter b" << endl;
+    cin >> b;
+    cout << "a^b is " << fastExponentiation(a, b) << endl;
+}
